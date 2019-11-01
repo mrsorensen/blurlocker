f87d2b5354c87875d6576e35374ac5b73ae93a98
Mr. Sorensen <mrsorensen@protonmail.com>
2019-11-01 14:25:38 +0100

    Added comments

--- blurlocker.sh
+++ blurlocker.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# Target resolution
 RES=1366x768
+# Location for temporary lockscreen wallpaper
 TMPBG=/tmp/screen.png
-LOCK=~/Pictures/lock.png
+# Location of the logo you want
+LOCK=~/Pictures/arch.png
 
+# Screenshot current screen
 scrot -o $TMPBG
+# Convert screenshot to blur
 convert $TMPBG -blur 0x20 -resize $RES $TMPBG
+# Paste logo in center of blurred screenshot
 convert $TMPBG $LOCK -gravity center -composite $TMPBG
+# Lock the screen with the custom image
 i3lock -i $TMPBG
